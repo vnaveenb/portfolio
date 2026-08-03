a2a15f4d1dbb82b34b64584a7d87f37325f2bfff
vnaveenb <naveenbusiraju@gmail.com>
2026-08-03 21:07:03 +0530

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -43,6 +43,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
 const server = app.listen(port, () => {
   console.log(`Portfolio running at http://localhost:${port}`);
 });
